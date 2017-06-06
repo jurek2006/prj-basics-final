c507f68105ca35ced226f97573f97fa3d5cab678
Jurek <jurek2006@gmail.com>
2017-06-06 15:21:27 +0200

    Moje rozwiązanie działania przycisków nawigacji i wyświetlania Recipes lub Shopping List - Sekcja 6 Wykład 77 - początek

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+	btnRecipesClicked: boolean = true;
+	btnShoppingListClicked: boolean = false;
+
+	onNavBtnClicked(btnClicked: {btn: string}){
+	// obsługa zdarzenia navBtnClicked emitowanego przez app-header
+	// na podstawie klikniętego przycisku w pasku nawigacji ustawiana jest wartość btnRecipesClickedi i btnShoppingListClicked
+
+		if(btnClicked.btn === "Recipes"){
+			this.btnRecipesClicked = true;
+			this.btnShoppingListClicked = false;
+		}
+		else if(btnClicked.btn === "Shopping List"){
+			this.btnRecipesClicked = false;
+			this.btnShoppingListClicked = true;
+		}
+
+	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+	// obiekt event emmitera używany do emisji zdarzenia
+	@Output() navBtnClicked = new EventEmitter<{btn: string}>();
+
+	onNavBtnClick(event: Event){
+	// metoda wywoływana przy kliknięciu w przycisk Recipes lub Shopping List na pasku nawigacji
+	// w parametrze event przekazywane jest zdarzenie - za jego pomocą z targetu można wyczytać treść przycisku
+
+		// emisja zdarzenia (jako btn przekazywana jest treść przyciskanego linka czyli Shopping List albo Recipes)
+		this.navBtnClicked.emit({btn: (<HTMLInputElement>event.target).innerText});
+	}
 
 }
