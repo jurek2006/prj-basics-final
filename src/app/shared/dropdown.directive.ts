84bcc5a7d1a187dda70da6c856b0b7f1e7e8532a
Jurek <jurek2006@gmail.com>
2017-06-14 18:45:44 +0200

    Building and Using a Dropdown Directive - Sekcja 7 Wykład 93

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+// dropdown.directive.ts
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+
+@Directive({
+	selector: '[appDropdown]'
+})
+export class DropdownDirective{
+	@HostBinding('class.open') isOpen = false;
+
+	@HostListener('click') toggleOpen(){
+  	this.isOpen = !this.isOpen;
+  }
+}
