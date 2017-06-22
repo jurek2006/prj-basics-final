499546a3685b5bf61ab8850365d8150185385473
Jurek <jurek2006@gmail.com>
2017-06-22 07:07:20 +0200

    Moje rozwiązanie do zadania w Adding the Shopping List Service - Sekcja 10 Wykład 107

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,26 +2,31 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+    // pobranie tablicy składników z serwisu shoppingListService
+    this.ingredients = this.shoppingListService.getIngredients();
+    // nasłuchiwanie, czy nastąpiła zmiana w tablicy ingredients w serwisie
+    this.shoppingListService.ingredientsChanged.subscribe(
+      () => {this.ingredients = this.shoppingListService.getIngredients();}
+    );
   }
 
   onShpListItemAdded(ingredient: Ingredient){
   // metoda obsługi zdarzenia shpListItemAdded - dodająca Ingredient z formularza do listy ingredients
-    this.ingredients.push(ingredient);
+    this.shoppingListService.addShoppingListItem(ingredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,23 @@
+// shopping-list.service.ts
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService{
 
+	ingredientsChanged = new EventEmitter<void>();
+
+	private ingredients: Ingredient[] = [
+	    new Ingredient('Apples', 5),
+	    new Ingredient('Tomatoes', 10),
+	];
+
+	getIngredients(): Ingredient[]{
+		return this.ingredients.slice();
+	}
+
+	addShoppingListItem(ingredient: Ingredient){
+	  // metoda dodająca Ingredient do listy ingredients
+	    this.ingredients.push(ingredient);
+	    this.ingredientsChanged.emit();
+	  }
 }
