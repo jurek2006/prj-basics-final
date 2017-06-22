3ed4d6314e34c5f47311447022d22d1cca82b61c
Jurek <jurek2006@gmail.com>
2017-06-22 13:13:42 +0200

    Adding Ingredients to Recipes - Sekcja 10 Wykład 109

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,14 +2,29 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 
 	// pole prywatne, niedostępne z zewnątrz
 	private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe2', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A Super-tasty Schnitzel - just awesome', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/1200px-Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big fat burger', 
+      'What else you need to say?', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Steak_burger_with_cheese_and_onion_rings.jpg/220px-Steak_burger_with_cheese_and_onion_rings.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2)
+      ])
   ];
 
   getRecipes(): Recipe[]{
